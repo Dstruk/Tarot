2af408091dfda0888508c08778f6fed1395ce16e
Dstruk <shehe18yeye@gmail.com>
2026-08-04 19:32:27 -0600

    Fix: Estructura KMP limpia y repositorios Wasm configurados

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,13 +8,21 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    // CAMBIO AQUÍ: Cambiamos de FAIL_ON_PROJECT_REPOS a PREFER_PROJECT
-    // Esto permite que el plugin de Kotlin Wasm añada el repositorio de Binaryen automáticamente.
+    // Cambiamos a PREFER_PROJECT para que el plugin de Kotlin pueda añadir sus repositorios de herramientas
     repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        
+        // Añadimos el repositorio de Binaryen explícitamente por si acaso
+        ivy {
+            url = uri("https://github.com/WebAssembly/binaryen/releases/download")
+            patternLayout {
+                artifact("[revision]/[artifact]-[revision]-[classifier].[ext]")
+            }
+            metadataSources { artifact() }
+        }
     }
 }
 
